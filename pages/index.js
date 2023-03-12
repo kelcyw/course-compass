8d920c3cd1dd6f78c0ed18537d482ecd6a9cac2e
kelcyw <kelcy.w.wang@gmail.com>
2023-03-12 09:14:33 -0700

    finished functionality for course map

--- pages/index.js
+++ pages/index.js
@@ -32,8 +32,8 @@ class Course {
 
 // Degree Class
 class Degree {
-    #degreeName;
-    #requirements;
+    degreeName;
+    requirements;
 
     constructor(degreeName, requirements) {
         this.degreeName = degreeName;
@@ -41,11 +41,11 @@ class Degree {
     }
 
     get getDegreeName() {
-        return this.#degreeName;
+        return this.degreeName;
     }
 
     get getRequirements() {
-        return this.#requirements;
+        return this.requirements;
     }
 }
 
@@ -54,8 +54,13 @@ function filterCoursesByName(name) {
 
     
     cs_degree.requirements.forEach((list) => {
+<<<<<<< Updated upstream
         list.forEach((course) => {
             if (!filtered.contains(course) && course.getCourseName()) {
+=======
+        list.item.forEach((course) => {
+            if (!filtered.contains(course) && course.getCourseName) {
+>>>>>>> Stashed changes
                 filtered.add(course);
             }
         })
@@ -106,7 +111,7 @@ class Student {
     }
 
     getUnfulfilledReqs() {
-        let requirements = this.degree.getRequirements();
+        let requirements = this.degree.getRequirements;
         let unfulfilled = [];
 
         requirements.forEach((path) => {
@@ -122,7 +127,7 @@ class Student {
     }
 
     getAvailableCourses() {
-        let requirements = this.degree.getRequirements();
+        let requirements = this.degree.getRequirements;
         let available = [];
 
         requirements.forEach((path) => {
@@ -144,6 +149,7 @@ class Student {
     }
 }
 
+<<<<<<< Updated upstream
 // Courses Initialization
 // first year
 let cs103 = new Course([], "CPSC", 103, 3);
@@ -201,13 +207,25 @@ var student = {
         ]
     }
 }
+=======
+let requirements = [
+    new Course([], "CPSC", 100, 3), 
+    new Course([], "CPSC", 103, 3), 
+    new Course([new Course([], "CPSC", 103, 3)], "CPSC", 107, 3), 
+    new Course([], "CPSC", 110, 4)
+]
+
+let csDegree = new Degree("B.Sc, Computer Science", requirements);
+
+var student = new Student("John", 555666777, [], csDegree);
+>>>>>>> Stashed changes
 
 function loadIndex() {
 
-    document.getElementById("nameField").innerHTML=student.name;
-    document.getElementById("idField").innerHTML=student.id;
-    document.getElementById("degreeField").innerHTML=student.degree.name;
-    document.getElementById("coursesField").innerHTML=student.takenCourses;  
+    document.getElementById("nameField").innerHTML=student.getStudentName;
+    document.getElementById("idField").innerHTML=student.getStudentID;
+    document.getElementById("degreeField").innerHTML=student.getStudentDegree.getDegreeName;
+    document.getElementById("coursesField").innerHTML=student.getStudentCourses;  
 
 }
 
@@ -218,34 +236,36 @@ function loadCourseMap() {
 
 function addCourse() {
     student.takenCourses.push("CPSC 110");
-    load();
+    loadIndex();
 }
 
 function loadMap() {
-    var requirements = student.degree.requirements;
+    var reqs = student.getDegree.getRequirements;
     
-    for (var i = 0, max = requirements.length; i < max; i++) {
+    for (var i = 0, max = reqs.length; i < max; i++) {
         var d = document.createElement('div');
         d.className = "form-check form-check-inline";
         d.id = i;
+        var l = document.createElement('label');
+        l.className = "form-check-label";
+        l.id = i + " label";
+        l.innerHTML = reqs[i].courseName + " " + reqs[i].courseCode;
         var c = document.createElement('input');
         c.className = "form-check-input";
         c.type = "checkbox";
         c.id = i + " checkbox"; 
 
-        c.innerHTML = '<label class="form-check-label" for="' +i+ ' checkbox">' 
-        + requirements[i].courseName + " " + requirements[i].courseCode
-        + '</label>';
-
-        // d.innerHTML = '<input class="form-check-input" type="checkbox" value="" id="courseCheckBox"><label class="form-check-label" for="flexCheckDefault">'
-        // + requirements[i].courseName + " " +
-        // + requirements[i].courseCode
-        // +'</label>';
-
         // function for comparing against taken courses here
+        for (var j = 0, max = reqs[i].prereqs.length; j < max; j++) {
+            var prereq = reqs[i].preqreqs[j]
+            if (!student.takenCourses.includes(prereq)) {
+                c.disabled = "true";  // check
+            }
+        }
 
         document.getElementById('form').appendChild(d);
-        document.getElementById(i).appendChild(c);
+        document.getElementById(i).appendChild(l);
+        document.getElementById(i + " label").appendChild(c);
     }
 }
 
